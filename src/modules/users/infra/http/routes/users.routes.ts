257b5d09b2b0f0451d666695777923fb1e245810
gustavo <guga-rd@hotmail.com>
2021-02-09 11:56:00 -0300

    Adição de controllers.

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,61 +1,23 @@
 import { Router } from 'express';
 import multer from 'multer';
 import uploadConfig from '@config/upload';
-import { container } from 'tsyringe';
-
-import CreateUserService from '@modules/users/services/CreateUserService';
-import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+import UsersController from '../controllers/UsersController';
+import AvatarController from '../controllers/AvatarController';
 
 const usersRouter = Router();
 const upload = multer(uploadConfig);
+const usersController = new UsersController();
+const avatarController = new AvatarController();
 
-usersRouter.post('/', async (request, response) => {
-    try {
-        const { name, email, password } = request.body;
-
-        const createUser = container.resolve(CreateUserService);
-
-        const user = await createUser.execute({ name, email, password });
-
-        const userWithoutPassword = {
-            id: user.id,
-            name: user.name,
-            email: user.email,
-            created_at: user.created_at,
-            updated_at: user.updated_at,
-        };
-
-        return response.json(userWithoutPassword);
-    } catch (err) {
-        return response.status(400).json({ error: err.message });
-    }
-});
+usersRouter.post('/', usersController.create);
 
 usersRouter.patch(
     '/avatar',
     ensureAuthenticated,
     upload.single('avatar'),
-    async (request, response) => {
-        const updateUserAvatar = container.resolve(UpdateUserAvatarService);
-
-        const user = await updateUserAvatar.execute({
-            user_id: request.user.id,
-            avatarFileName: request.file.filename,
-        });
-
-        const userWithoutPassword = {
-            id: user.id,
-            name: user.name,
-            email: user.email,
-            avatar: user.avatar,
-            created_at: user.created_at,
-            updated_at: user.updated_at,
-        };
-
-        return response.json({ user: userWithoutPassword });
-    },
+    avatarController.update,
 );
 
 export default usersRouter;
