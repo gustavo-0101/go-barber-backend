dc84f2eec8156f610910fb6991b6f2544c081460
gustavo <guga-rd@hotmail.com>
2021-02-09 16:17:21 -0300

    TEste de data.

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,5 @@
+import AppError from '@shared/errors/AppError';
+
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
@@ -17,7 +19,24 @@ describe('CreateAppointmentService', () => {
         expect(appointment.provider_id).toBe('123123');
     });
 
-    // it('should not be able to create appointments in the same date', () => {
-    //     expect(1 + 2).toBe(3);
-    // });
+    it('should not be able to create appointments in the same date', async () => {
+        const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+        const createAppointment = new CreateAppointmentService(
+            fakeAppointmentsRepository,
+        );
+
+        const appointmentDate = new Date(2020, 4, 10, 11);
+
+        await createAppointment.execute({
+            date: appointmentDate,
+            provider_id: '123123',
+        });
+
+        expect(
+            createAppointment.execute({
+                date: appointmentDate,
+                provider_id: '123123',
+            }),
+        ).rejects.toBeInstanceOf(AppError);
+    });
 });
