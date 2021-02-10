ec5e25b9845766a905998caa4578ff71812ff4dc
gustavo <guga-rd@hotmail.com>
2021-02-10 17:33:51 -0300

    Criação de provider de hash para senha.

--- src/modules/users/services/AuthenticateUserService.ts
+++ src/modules/users/services/AuthenticateUserService.ts
@@ -1,55 +1,53 @@
-import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
+import authConfig from '@config/auth';
 import { injectable, inject } from 'tsyringe';
-
-import User from '@modules/users/infra/typeorm/entities/User';
-import IUsersRepository from '@modules/users/repositories/iUserRepository';
-
 import AppError from '@shared/errors/AppError';
-import authConfig from '../../../config/auth';
+
+import User from '../infra/typeorm/entities/User';
+import IUsersRepository from '../repositories/iUserRepository';
+import IHashProvider from '../providers/HashProvider/models/iHashProvider';
 
 interface IRequest {
     email: string;
     password: string;
 }
-
 interface IResponse {
     user: User;
     token: string;
 }
-
 @injectable()
 class AuthenticateUserService {
     constructor(
         @inject('UsersRepository')
         private usersRepository: IUsersRepository,
+
+        @inject('HashProvider')
+        private hashProvider: IHashProvider,
     ) {}
 
     public async execute({ email, password }: IRequest): Promise<IResponse> {
         const user = await this.usersRepository.findByEmail(email);
-
         if (!user) {
             throw new AppError('Incorrect email/password combination', 401);
         }
 
-        const passwordMatched = await compare(password, user.password);
+        const passwordMatched = await this.hashProvider.compareHash(
+            password,
+            user.password,
+        );
 
         if (!passwordMatched) {
             throw new AppError('Incorrect email/password combination', 401);
         }
-
         const { secret, expiresIn } = authConfig.jwt;
-
         const token = sign({}, secret, {
             subject: user.id,
             expiresIn,
         });
-
         return {
             user,
             token,
         };
     }
 }
-
 export default AuthenticateUserService;
