49cdf7b86a8f5157fd879512dfa415de152539f3
Gustavo Dias <guga-rd@hotmail.com>
2021-03-08 20:01:14 -0300

    Adição de provider de email.

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -4,8 +4,14 @@ import DiskStorageProvider from './StorageProvider/implementations/DiskStoragePr
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 
 import IEmailProvider from './EmailProvider/models/IEmailProvider';
+import EtherealMailProvider from './EmailProvider/implementations/EtherealMailProvider';
 
 container.registerSingleton<IStorageProvider>(
     'StorageProvider',
     DiskStorageProvider,
+);
+
+container.registerSingleton<IEmailProvider>(
+    'EmailProvider',
+    EtherealMailProvider,
 );
